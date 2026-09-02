e6d595297b799749a9ae359fcb7ec54866ed19e0
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-02 23:36:58 +0530

    Time: 2 ms (83.74%), Space: 42.2 MB (25.88%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; (i * i) <= num; i++) {
+            if ((num % i) == 0) {
+                sum = sum + i;
+                if ((num / i) != i) {
+                    sum = sum + (num / i);
+                }
+            }
+        }
+        return (sum - num) == num;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
