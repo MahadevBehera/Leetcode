0d07a92fdb616e9e998aa250d115003b50da3e62
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-18 00:08:41 +0530

    Two odd Occurring - GFG

---
+++ 705589-two-odd-occuring-GFG/705589-two-odd-occuring.java
@@ -0,0 +1,36 @@
+class Solution {
+	ArrayList<Integer> twoOddNum(int[] arr) {
+		int x = 0;
+		for (int n : arr) {
+			x = x ^ n;
+		}
+		
+		int k = x & (~(x - 1));
+		int res1 = 0, res2 = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if ((arr[i] & k) != 0) {
+				res1 = res1 ^ arr[i];
+			}
+			else {
+				res2 = res2 ^ arr[i];
+			}
+		}
+		
+		ArrayList<Integer> result = new ArrayList<>();
+		if (res1 > res2) {
+			result.add(res1);
+			result.add(res2);
+		}
+		else {
+			result.add(res2);
+			result.add(res1);
+		}
+		
+		return result;
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
