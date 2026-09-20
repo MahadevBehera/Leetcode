3220d32a3f56f609c6614be5a0552f3b8f267ec0
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-20 19:53:26 +0530

    Bit Difference - GFG

---
+++ 701160-bit-difference-GFG/701160-bit-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+	public static int countBitsFlip(int a, int b) {
+		int n = a ^ b;
+		int count = 0;
+		while (n > 0) {
+			n = n & (n - 1); // going through only set bits
+			count++;
+		}
+		return count;
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
