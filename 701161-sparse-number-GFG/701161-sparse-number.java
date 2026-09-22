2e74621ce392e3881d9b4b9ef8324fab8acd9620
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-22 11:02:06 +0530

    Sparse Number - Time: O(1), Space: O(1) - GFG

--- 701161-sparse-number-GFG/701161-sparse-number.java
+++ 701161-sparse-number-GFG/701161-sparse-number.java
@@ -1,13 +1,12 @@
 class Solution {
 	public static boolean isSparse(int n) {
-		int count = 0;
-		while (n != 0) {
-			n = n & (n << 1);
-			count++;
-			if (count >= 2)
-				return false;
-		}
-		
+		// n is not a Sparse number if there is set bit in AND of n and n/2 (n>>1).
+		// Means after right shift n by 1 and doing AND operation between n with result,
+		// then if there is max 1 consecutive set bit, the final result after AND will be zero.
+		// Otherwise the result will contain 1's, mean value will be either 1 or more than 1.
+		if((n & (n >> 1)) >= 1) {
+		    return false;
+		} 
 		return true;
 	}
 }
