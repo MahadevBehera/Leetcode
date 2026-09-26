654fa79bbd94598baf646776de1daa317113e83f
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-26 14:58:06 +0530

    Recursively Sum n Numbers - GFG

---
+++ 701576-recursively-sum-n-numbers-GFG/701576-recursively-sum-n-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int recursiveSum(int n) {
+        if (n == 0)
+			return 0;
+        return n + recursiveSum(n - 1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
