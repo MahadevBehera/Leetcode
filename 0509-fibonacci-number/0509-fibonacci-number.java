85f78618355c525754a4bc2c352d6e2ce16560a4
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-26 11:15:16 +0530

    Time: 0 ms (100%) - Using Memorization - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0)
+        if (n == 0 || n == 1)
             return n;
-        int arr[] = new int[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
 
+        int a = 0;
+        int b = 1;
+        int c = 1;
         for (int i = 2; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
+            c = a + b;
+            a = b;
+            b = c;
         }
 
-        return arr[n];
-
+        return c; // Using Memorization
     }
 }
 
