4c4a4756b1ae1366310a2d72a52b53f2499b7518
Mahadev Behera <50512391+MahadevBehera@users.noreply.github.com>
2026-09-26 15:00:01 +0530

    Sum of Natural Numbers - with formula - GFG

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((n * (n+1))/2);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
